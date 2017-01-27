7cf1b67be18e8544bbc493c74a5ced14643bb110
Alex Davies <alexd@asana.com>
2017-01-27 17:51:37 +0100

    kinda works for tasks which have all their values!

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ var client = asana.Client.create({
     asanaBaseUrl: "https://localhost.asana.com:8180/"
 }).useAccessToken(pat);
 
-console.log(projects)
-
 projects.forEach(function(project_id) {
     client.projects.findById(project_id, {
-        opt_fields: "custom_field_settings.custom_field.description"
+        // TODO name is only here for debugging
+        opt_fields: "custom_field_settings.custom_field.name,custom_field_settings.custom_field.description"
     }).then(function(project) {
 
         var fields = project.custom_field_settings.map(function(cfs) {
@@ -32,21 +31,116 @@ projects.forEach(function(project_id) {
             opt_fields: "name,completed,custom_fields"
         }).then(function(tasks_collection) {
             tasks_collection.stream().on("data", function(task) {
-                console.log(task)
+                var task_field_values_by_name = {};
+                task.custom_fields.forEach(function(custom_field) {
+                   task_field_values_by_name[custom_field.name] = custom_field;
+                });
 
-                if (task.id === 172596197007857) {
-                    var weighted_value = task.custom_fields.find(function(cf) {
-                        return cf.id === 158710296730351;
-                    })
-                    console.log(weighted_value)
+                var calc = function(formula_object) {
+                    switch (typeof formula_object) {
+                        case "object":
+                            console.assert(Array.isArray(formula_object), "Invalid formula field", formula_object);
 
-                    weighted_value.number_value = 1;
+                            // Recursive case, this is another formula
+                            var xs = formula_object;
+                            switch (xs.length) {
+                                case 2:
+                                    switch (xs[0]) {
+                                        case "!":
+                                            return !calc(xs[1]);
+                                        case "-":
+                                            return -calc(xs[1]);
+                                        default:
+                                            throw new Error("Couldn't parse expression with 2 tokens");
+                                    }
+                                case 3:
+                                    switch (xs[1]) {
+                                        case "+":
+                                            return calc(xs[0]) + calc(xs[2]);
+                                        case "-":
+                                            return calc(xs[0]) - calc(xs[2]);
+                                        case "*":
+                                            return calc(xs[0]) * calc(xs[2]);
+                                        case "/":
+                                            return calc(xs[0]) / calc(xs[2]);
+                                        case "=":
+                                        case "==":
+                                        case "===":
+                                            return calc(xs[0]) === calc(xs[2]);
+                                        case "!=":
+                                        case "!==":
+                                        case "<>": //Yeah basic!
+                                            return calc(xs[0]) !== calc(xs[2]);
+                                        case ">":
+                                            return calc(xs[0]) > calc(xs[2]);
+                                        case ">=":
+                                            return calc(xs[0]) >= calc(xs[2]);
+                                        case "<":
+                                            return calc(xs[0]) < calc(xs[2]);
+                                        case "<=":
+                                            return calc(xs[0]) <= calc(xs[2]);
+                                        case "&&":
+                                            return calc(xs[0]) && calc(xs[2]);
+                                        case "||":
+                                            return calc(xs[0]) || calc(xs[2]);
+                                        default:
+                                            throw new Error("Couldn't parse expression with 3 tokens");
+                                    }
+                                case 5:
+                                    if (xs[1] === "?" && xs[3] === ":") {
+                                        // Ternary if operator. We use javascript's truthiness
+                                        return calc(xs[0]) ? calc(xs[2]) : calc(xs[4]);
+                                    } else {
+                                        throw new Error("Couldn't parse expression with 5 tokens");
+                                    }
+                                default:
+                                    throw new Error("Couldn't parse expression, unused number of tokens");
+                            }
 
-                    var new_custom_fields_on_task = {};
-                    new_custom_fields_on_task[weighted_value.id] = 1;
+                        case "string":
+                            // A reference to another custom field, hopefully
+                            // TODO make sure the other property got calculated before this one?
+                            // Could just rely on it running repetitively
+                            var field_value = task_field_values_by_name[formula_object];
+                            if (field_value === undefined) {
+                                // Referenced a field that this task doesn't have, use NaN to unset any calculated value
+                                return NaN;
+                            } else if (field_value.number_value !== undefined) {
+                                return field_value.number_value;
+                            } else if (field_value.text ) {
+                                // Assume everything is a date and return the number of days
+                                return Date.parse(field_value.text) / Date.MILLISECONDS_PER_DAY;
+                            } else {
+                                throw new Error("Unexpected type of custom property", field_value);
+                            }
 
+                        case "number":
+                            // A literal
+                            return formula_object;
 
-                    client.tasks.update(task.id, { custom_fields: new_custom_fields_on_task })
+                        default:
+                    }
+                };
+
+                var new_custom_field_values_to_write_to_task = {};
+                var any_new_custom_field_values_to_write = false;
+                formula_fields.forEach(function(field) {
+                    var formula_json = field.description.substring(1);
+                    var formula_object = JSON.parse(formula_json);
+                    var formula_value = calc(formula_object);
+                    console.log("Formula value", task.name, field.name, formula_value);
+                    if (!isNaN(formula_value)) {
+                        // TODO defend against formula fields not being numbers
+                        var existing_value = task_field_values_by_name[field.name].number_value;
+                        if (existing_value !== formula_value) {
+                            new_custom_field_values_to_write_to_task[field.id] = formula_value;
+                            any_new_custom_field_values_to_write = true;
+                        }
+                    }
+                });
+
+                if (any_new_custom_field_values_to_write) {
+                    client.tasks.update(task.id, {custom_fields: new_custom_field_values_to_write_to_task});
                 }
             });
         });
